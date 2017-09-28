610b7f1703d6c998c00657bc4abac6da4a5d62fa
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-28 09:40:44 +0200

    Done

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+  $hobbies = [
+    "Hacking",
+    "Drinking",
+    "Streaking",
+    "Doing nothing",
+    "Sleeping",
+    "LARPing",
+    "Stalking",
+  ];
+  foreach ($hobbies as $value) {
+    echo $value;
+    echo "\n";
+  }
 
-
-?>
+  foreach($hobbies as $value): ?>
+   <p> <?php echo $value; ?> </p>
+  <?php endforeach; ?>
